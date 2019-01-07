787204408dbd23c085dbd3bdc6d27dc3d3aa9fba
iClaude <agostini.claudio1@gmail.com>
2019-01-07 15:28:34 +0100

    define Event model class to match Firestore data

--- app/src/main/java/iclaude/festivaleconomia2019/model/Event.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/Event.kt
@@ -1,27 +1,30 @@
 package iclaude.festivaleconomia2019.model
 
-import org.threeten.bp.ZonedDateTime
+
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
 
 
 data class Event(
     val id: String,
     val title: String,
-    val startTime: ZonedDateTime,
-    val endTime: ZonedDateTime,
+    @ServerTimestamp val startTime: Date,
+    @ServerTimestamp val endTime: Date,
     val location: Location,
     val description: String,
-    val tags: List<Tag>,
-    val organizers: List<Organizer>,
+    val tags1: List<String>,
+    val tags2: List<String>,
+    val organizers: List<String>,
 
-    val relatedEvents: Set<Event>?,
+    val relatedEvents: List<String>?,
     val photoUrl: String?,
     val sessionUrl: String?,
     val youtubeUrl: String?
 )
 
 fun Event.isLive(): Boolean {
-    val now = ZonedDateTime.now()
-    return startTime <= now && endTime >= now
+    val now = System.currentTimeMillis()
+    return startTime.time <= now && endTime.time >= now
 }
 
 fun Event.hasPhotoUrl(): Boolean = photoUrl?.isNotBlank() ?: false
@@ -30,4 +33,4 @@ fun Event.hasYoutubeUrl(): Boolean = youtubeUrl?.isNotBlank() ?: false
 fun Event.hasRelatedEvents(): Boolean = relatedEvents?.isNotEmpty() ?: false
 
 val Event.duration: Long
-    get() = endTime.toInstant().toEpochMilli() - startTime.toInstant().toEpochMilli()
+    get() = endTime.time - startTime.time
--- app/src/main/java/iclaude/festivaleconomia2019/model/Location.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/Location.kt
@@ -1,13 +1,13 @@
 package iclaude.festivaleconomia2019.model
 
+import com.google.firebase.firestore.GeoPoint
+
 /*
     Location of an event (session, conference, etc.).
  */
 
 data class Location(
-    val id: String,
     val name: String,
-    val coordinates: Coordinates
+    val details: String,
+    val coordinates: GeoPoint
 )
-
-data class Coordinates(val latitude: Double, val longitude: Double)
