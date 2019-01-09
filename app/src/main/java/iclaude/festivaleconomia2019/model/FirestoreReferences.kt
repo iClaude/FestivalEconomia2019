0b5b8ba1e5346921fc8eca792032aed936ce2bce
iClaude <agostini.claudio1@gmail.com>
2019-01-09 13:46:27 +0100

    create utility object with constants to Firestore references

---
+++ app/src/main/java/iclaude/festivaleconomia2019/model/FirestoreReferences.kt
@@ -0,0 +1,8 @@
+package iclaude.festivaleconomia2019.model
+
+object FirestoreReferences {
+    const val LOCATIONS = "events/festival_economia_2019/locations"
+    const val ORGANIZERS = "events/festival_economia_2019/organizers"
+    const val SESSIONS = "events/festival_economia_2019/sessions"
+    const val USERS = "events/festival_economia_2019/users"
+}
