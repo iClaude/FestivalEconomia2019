996c0db1f730b899d1a7121c98f4af3dd03da4c2
iClaude <agostini.claudio1@gmail.com>
2019-01-10 14:50:37 +0100

    replace agenda icon with map icon (animated) in the BottomNavigationView

--- app/src/main/java/iclaude/festivaleconomia2019/ui/MainActivity.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/MainActivity.kt
@@ -1,12 +1,8 @@
 package iclaude.festivaleconomia2019.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
 import iclaude.festivaleconomia2019.R
-import iclaude.festivaleconomia2019.model.Location
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,22 +11,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun testFirestore(v: View) {
-        val db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("events").document("festival_economia_2019").collection("locations").document("03")
-        val myTextView = findViewById<TextView>(R.id.textView)
 
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    val event = document.toObject(Location::class.java)
-                    myTextView.text = event.toString()
-                } else {
-                    myTextView.text = "no document found"
-                }
-            }
-            .addOnFailureListener { exception ->
-                myTextView.text = "exception occured"
-            }
-    }
 }
