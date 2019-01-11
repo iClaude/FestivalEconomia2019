a8a99ff74437cab57dd110eabd5fa216c53d3518
iClaude <agostini.claudio1@gmail.com>
2019-01-11 14:30:43 +0100

    refactor model class Location

--- app/src/main/java/iclaude/festivaleconomia2019/model/Location.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/Location.kt
@@ -1,15 +1,14 @@
 package iclaude.festivaleconomia2019.model
 
-import com.google.firebase.firestore.GeoPoint
-
 /*
     Location of an event (session, conference, etc.).
  */
 
 data class Location(
-    val id: String = "",
-    val name: String = "",
+    val id: String,
+    val name: String,
     val displayString: String? = null,
-    val description: String = "",
-    val coordinates: GeoPoint = GeoPoint(0.0, 0.0)
+    val description: String,
+    val lat: Double,
+    val lng: Double
 )
