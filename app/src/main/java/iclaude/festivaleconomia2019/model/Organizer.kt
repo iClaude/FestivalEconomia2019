b9e510fe689eb171546fd2782e209c30da816fe7
iClaude <agostini.claudio1@gmail.com>
2019-01-11 15:29:48 +0100

    refactor model class Organizer

--- app/src/main/java/iclaude/festivaleconomia2019/model/Organizer.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/Organizer.kt
@@ -6,21 +6,20 @@ package iclaude.festivaleconomia2019.model
 
 data class Organizer(
     // required data
-    val id: String = "",
-    val name: String = "",
-    val bio: String = "",
-    val sessions: List<String> = emptyList(), // list of ids of Session(s) belonging to this Organizer
+    val id: String,
+    val name: String,
+    val bio: String,
     // optional data
-    val company: String? = null,
-    val photoUrl: String? = null,
-    val websiteUrl: String? = null,
-    val twitterUrl: String? = null,
-    val linkedInUrl: String? = null,
-    val facebookUrl: String? = null
+    val company: String?,
+    val thumbnailUrl: String?,
+    val websiteUrl: String?,
+    val twitterUrl: String?,
+    val linkedInUrl: String?,
+    val facebookUrl: String?
 )
 
 fun Organizer.hasCompany() = company?.isNotBlank() ?: false
-fun Organizer.hasPhotoUrl() = photoUrl?.isNotBlank() ?: false
+fun Organizer.hasThumbnailUrl() = thumbnailUrl?.isNotBlank() ?: false
 fun Organizer.hasWebsiteUrl() = websiteUrl?.isNotBlank() ?: false
 fun Organizer.hastwitterUrl() = twitterUrl?.isNotBlank() ?: false
 fun Organizer.haslinkedInUrl() = linkedInUrl?.isNotBlank() ?: false
