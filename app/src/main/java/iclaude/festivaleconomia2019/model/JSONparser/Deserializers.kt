3913ea741003490fe1037276c375b3c3894cf017
iClaude <agostini.claudio1@gmail.com>
2019-01-15 20:21:17 +0100

    complete json deserializers classes for model data classes

--- app/src/main/java/iclaude/festivaleconomia2019/model/JSONparser/Deserializers.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/JSONparser/Deserializers.kt
@@ -4,8 +4,10 @@ import android.graphics.Color.parseColor
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import iclaude.festivaleconomia2019.model.data_classes.Location
 import iclaude.festivaleconomia2019.model.data_classes.Organizer
+import iclaude.festivaleconomia2019.model.data_classes.Session
 import iclaude.festivaleconomia2019.model.data_classes.Tag
 import java.lang.reflect.Type
 
@@ -23,6 +25,40 @@ class LocationDeserializer : JsonDeserializer<Location> {
                 lng = obj.get("lng").asDouble
 
         )
+    }
+}
+
+class SessionDeserializer : JsonDeserializer<Session> {
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Session {
+        val obj = json?.asJsonObject!!
+
+        val tags1 = getListFromJsonArray(obj, "tags1")
+        val tags2 = getListFromJsonArray(obj, "tags2")
+        val organizers = getListFromJsonArray(obj, "organizers")
+        val relatedSessions = getListFromJsonArray(obj, "relatedSessions")
+
+        return Session(
+                id = obj.get("id").asString,
+                title = obj.get("title").asString,
+                startTimestamp = obj.get("startTimestamp").asLong,
+                endTimestamp = obj.get("endTimestamp").asLong,
+                location = obj.get("location").asString,
+                description = obj.get("description").asString,
+                tags1 = tags1.toList(),
+                tags2 = tags2.toList(),
+                organizers = organizers.toList(),
+                relatedSessions = relatedSessions.toList(),
+                photoUrl = obj.get("photoUrl")?.asString,
+                sessionUrl = obj.get("sessionUrl")?.asString,
+                youtubeUrl = obj.get("youtubeUrl")?.asString
+        )
+    }
+
+    private fun getListFromJsonArray(obj: JsonObject, key: String): List<String> {
+        val array = obj.get(key).asJsonArray
+        val stringList = ArrayList<String>()
+        array.mapTo(stringList) { it.asString }
+        return stringList
     }
 }
 
--- app/src/main/java/iclaude/festivaleconomia2019/model/JSONparser/JSONparser.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/model/JSONparser/JSONparser.kt
@@ -1,12 +1,21 @@
 package iclaude.festivaleconomia2019.model.JSONparser
 
 import com.google.gson.GsonBuilder
+import iclaude.festivaleconomia2019.model.data_classes.Location
+import iclaude.festivaleconomia2019.model.data_classes.Organizer
+import iclaude.festivaleconomia2019.model.data_classes.Session
+import iclaude.festivaleconomia2019.model.data_classes.Tag
 import java.io.InputStream
 
 object JSONparser {
     fun parseEventData(inputStream: InputStream): EventData {
         val jsonReader = com.google.gson.stream.JsonReader(inputStream.reader())
-        val gson = GsonBuilder().create()
+        val gson = GsonBuilder()
+                .registerTypeAdapter(Location::class.java, LocationDeserializer())
+                .registerTypeAdapter(Session::class.java, SessionDeserializer())
+                .registerTypeAdapter(Organizer::class.java, OrganizerDeserializer())
+                .registerTypeAdapter(Tag::class.java, TagDeserializer())
+                .create()
         return gson.fromJson(jsonReader, EventData::class.java)
     }
 }
