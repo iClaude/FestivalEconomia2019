13f1d52924375c71b43e9d186075c93727b4b092
iClaude <agostini.claudio1@gmail.com>
2019-01-20 13:13:32 +0100

    setup basic working map for MapFragment

--- app/src/main/java/iclaude/festivaleconomia2019/ui/MapFragment.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/MapFragment.kt
@@ -5,17 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import iclaude.festivaleconomia2019.R
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMapView: MapView
+    private var mGoogleMap: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
+        mMapView = root.findViewById<MapView>(R.id.mapView)
+        mMapView.onCreate(savedInstanceState)
+        mMapView.getMapAsync(this)
+
+        return root
     }
 
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView.onLowMemory()
+    }
+
+    override fun onMapReady(gMap: GoogleMap?) {
+        mGoogleMap = gMap
+    }
 }
