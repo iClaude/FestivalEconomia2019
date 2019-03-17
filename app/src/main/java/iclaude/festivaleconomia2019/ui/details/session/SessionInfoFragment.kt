639f21ebfb103391f31ec3b08b8283d663e23c3a
iClaude <agostini.claudio1@gmail.com>
2019-03-17 13:45:36 +0100

    improve animations for navigating between related sessions

--- app/src/main/java/iclaude/festivaleconomia2019/ui/details/session/SessionInfoFragment.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/details/session/SessionInfoFragment.kt
@@ -66,6 +66,8 @@ class SessionInfoFragment : Fragment() {
                 val options = navOptions {
                     anim {
                         enter = R.anim.slide_in_right
+                        exit = R.anim.slide_out_left
+                        popEnter = R.anim.slide_in_left
                         popExit = R.anim.slide_out_right
                     }
                 }
