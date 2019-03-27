9209613da6b736d564f8be7f920ea706c9e9c621
iClaude <agostini.claudio1@gmail.com>
2019-03-27 14:53:07 +0100

    use NavigationUI to associate BottomNavigationView with NavController

--- app/src/main/java/iclaude/festivaleconomia2019/ui/MainActivity.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import iclaude.festivaleconomia2019.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,17 +23,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupBottomNavMenu(navController: NavController) {
-        bottomNav.setOnNavigationItemSelectedListener { menuItem ->
-            if (menuItem.itemId != bottomNav.selectedItemId) {
-
-                when (menuItem.itemId) {
-                    R.id.containerInfoFragment -> navController.navigate(R.id.action_global_containerInfoFragment)
-                    R.id.containerSessionsFragment -> navController.navigate(R.id.action_global_containerSessionsFragment)
-                    R.id.mapFragment -> navController.navigate(R.id.action_global_mapFragment)
-                }
-            }
-            true
-        }
-        bottomNav.menu.findItem(R.id.containerInfoFragment).setChecked(true)
+        bottomNav?.setupWithNavController(navController)
     }
 }
