af6ac0e0f9f12482a93b156213ca2ca378f3a7f4
iClaude <agostini.claudio1@gmail.com>
2019-04-15 15:44:34 +0200

    add some faqs and move Lottie view at the top

--- app/src/main/java/iclaude/festivaleconomia2019/ui/info/faq/FAQFragment.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/info/faq/FAQFragment.kt
@@ -2,6 +2,7 @@ package iclaude.festivaleconomia2019.ui.info.faq
 
 import android.os.Bundle
 import android.text.SpannableString
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -43,6 +44,38 @@ class FAQFragment : Fragment(), FAQView {
      */
 
     override fun showCarInfo(spannableString: SpannableString) {
-        ccCar.tvCardDescription.text = spannableString
+        ccCar.tvCardDescription.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+
+        }
+    }
+
+    override fun showParkingCoachesInfo(spannableString: SpannableString) {
+        ccParkingCoaches.tvCardDescription.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+
+    override fun showParkingVansInfo(spannableString: SpannableString) {
+        ccParkingVans.tvCardDescription.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+
+    override fun showUsefulNumbersInfo(spannableString: SpannableString) {
+        ccUsefulNumbers.tvCardDescription.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+        }
+    }
+
+    override fun showHandicapInfo(spannableString: SpannableString) {
+        ccHandicap.tvCardDescription.apply {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 }
--- app/src/main/java/iclaude/festivaleconomia2019/ui/info/faq/FAQPresenter.kt
+++ app/src/main/java/iclaude/festivaleconomia2019/ui/info/faq/FAQPresenter.kt
@@ -12,6 +12,10 @@ import iclaude.festivaleconomia2019.ui.utils.buildSpannableString
 
 interface FAQView {
     fun showCarInfo(spannableString: SpannableString)
+    fun showParkingCoachesInfo(spannableString: SpannableString)
+    fun showParkingVansInfo(spannableString: SpannableString)
+    fun showUsefulNumbersInfo(spannableString: SpannableString)
+    fun showHandicapInfo(spannableString: SpannableString)
 
 }
 
@@ -20,7 +24,10 @@ class FAQPresenter(private var faqView: FAQView, private var mContext: Context) 
     // Display all the formatted data in the associated View.
     fun displayData() {
         faqView.showCarInfo(createCarInfoText())
-
+        faqView.showParkingCoachesInfo(createParkingCoachesText())
+        faqView.showParkingVansInfo(createParkingVansText())
+        faqView.showUsefulNumbersInfo(createUsefulNumbersText())
+        faqView.showHandicapInfo(createHandicapInfoText())
     }
 
     /**
@@ -39,6 +46,120 @@ class FAQPresenter(private var faqView: FAQView, private var mContext: Context) 
                 clickableSpan {
                     context = mContext
                     url = "http://www.viaggiareintrentino.it"
+                }
+            }
+        }
+    }
+
+    private fun createParkingCoachesText(): SpannableString {
+        val str = mContext.getString(R.string.faq_parkingcoaches_description)
+
+        return buildSpannableString {
+            string = str
+
+            span {
+                start = str.indexOf("Trento-Monte Bondone")
+                end = start + 20
+                clickableSpan {
+                    context = mContext
+                    url = "http://www.comune.trento.it/content/view/full/221253"
+                }
+            }
+        }
+    }
+
+    private fun createParkingVansText(): SpannableString {
+        val str = mContext.getString(R.string.faq_parkingvans_description)
+
+        return buildSpannableString {
+            string = str
+
+            span {
+                start = str.indexOf("http://www.trentinomobilita.it")
+                end = start + 30
+                clickableSpan {
+                    context = mContext
+                    url = "http://www.trentinomobilita.it"
+                }
+            }
+        }
+    }
+
+    private fun createUsefulNumbersText(): SpannableString {
+        val chemistsListStr = mContext.getString(R.string.faq_usefulenumbers_chemists_list)
+        val chemistsMapStr = mContext.getString(R.string.faq_usefulenumbers_chemists_map)
+        val hospitalStr = mContext.getString(R.string.faq_usefulenumbers_hospital)
+        val emergencyStr = mContext.getString(R.string.faq_usefulenumbers_emergency_medical)
+        val str = mContext.getString(
+            R.string.faq_usefulnumbers_description,
+            chemistsListStr,
+            chemistsMapStr,
+            hospitalStr,
+            emergencyStr
+        )
+
+        return buildSpannableString {
+            string = str
+
+            span {
+                start = str.indexOf(chemistsListStr)
+                end = start + chemistsListStr.length
+                clickableSpan {
+                    context = mContext
+                    url = mContext.getString(R.string.faq_usefulenumbers_chemists_list_link)
+                }
+            }
+
+            span {
+                start = str.indexOf(chemistsMapStr)
+                end = start + chemistsMapStr.length
+                clickableSpan {
+                    context = mContext
+                    url = mContext.getString(R.string.faq_usefulenumbers_chemists_map_link)
+                }
+            }
+
+            span {
+                start = str.indexOf(hospitalStr)
+                end = start + hospitalStr.length
+                clickableSpan {
+                    context = mContext
+                    url = mContext.getString(R.string.faq_usefulenumbers_hospital_link)
+                }
+            }
+
+            span {
+                start = str.indexOf(emergencyStr)
+                end = start + emergencyStr.length
+                clickableSpan {
+                    context = mContext
+                    url = mContext.getString(R.string.faq_usefulenumbers_emergency_medical_link)
+                }
+            }
+        }
+    }
+
+    private fun createHandicapInfoText(): SpannableString {
+        val str = mContext.getString(R.string.faq_handicap_description)
+
+        return buildSpannableString {
+            string = str
+
+            span {
+                start = str.indexOf("www.handicrea.it")
+                end = start + 16
+                clickableSpan {
+                    context = mContext
+                    url = "http://www.handicrea.it/"
+                }
+            }
+
+            span {
+                start = str.indexOf("www.ttesercizio.it.")
+                end = start + 18
+                clickableSpan {
+                    context = mContext
+                    url = "https://www.trentinotrasporti.it/"
                 }
             }
         }
